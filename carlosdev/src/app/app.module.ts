443dd1f9313b1dc3ac861ad9ea74c1e9e9382449
carlos-chourio <carlos18daniel@gmail.com>
2019-08-27 22:20:31 -0400

    Add real links in nav-bar and add routing functionality
    Create a new Post Component (Empty)

--- carlosdev/src/app/app.module.ts
+++ carlosdev/src/app/app.module.ts
@@ -5,17 +5,22 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    ContactComponent
+    ContactComponent,
+    HomeComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      { path:'contact', component:ContactComponent }
+      { path:'contact', component:ContactComponent },
+      { path:'', component:HomeComponent }
     ])
   ],
   providers: [],
